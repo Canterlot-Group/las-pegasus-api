2589079c271e29d2f5268c401c44312199c5db57
noskla <alis@krutt.org>
2020-08-27 12:25:51 +0200

    make the 404 error more obvious

--- src/index.mjs
+++ src/index.mjs
@@ -181,7 +181,7 @@ Router.delete('/show/:show_id', (...args) => Show.delete(...args));
 
 
 app.use(Router);
-app.use((req, res) => res.status(404).json({ stat: 'Err', err: 'not found' }));
+app.use((req, res) => res.status(404).json({ stat: 'Err', err: 'route not found' }));
 
 server.listen(process.env.PORT || config.http_port || 8000);
 export default app;
